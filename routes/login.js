fadf01b36193e63fadf364ef8122950d75786b31
Darshil Shah <shahdarshil622@gmail.com>
2021-05-26 16:31:01 +0530

    authentication complete

--- routes/login.js
+++ routes/login.js
@@ -7,14 +7,14 @@ router.use(express.urlencoded({extended: true}));
 
 router.use(express.json());
 
-router.get('/form', function(req, res, next) { 
+router.get('/form', function(req, res, next){ 
 res.render('login.ejs'); 
 });
 
-
+var db2=require('../login_database');
 router.post('/verify',(req,res)=>{
-	var db2=require('../login_database');
 	
+
 	const id = req.body.id;
 	const password = req.body.password;
 
@@ -28,16 +28,23 @@ router.post('/verify',(req,res)=>{
 			if(typeof(data[0]) != "undefined"){
 				if(password === data[0].password){
 				console.log("password matched");
-				res.send("Login successful");
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.write("Login successful");
+				res.write("<p>Welcome to our system!<p>");
+				res.end();
 				}
 				else{
 				console.log("password not match");
-				res.send('Password wrong');
+				res.write('Password wrong'); 
+				res.end();
 				}
 			}
 			else
 			{
-				res.send("Invalid roll no");
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.write("<p>Invalid roll no </p>");
+				res.write('<button><a href="http://localhost:3000/login/form">Back to Login</a></button>');
+				res.end();
 			}		
 		}
 	});
